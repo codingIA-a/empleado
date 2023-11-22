67a95a818318f3ad6f7fe5dfd76df042ccf3c64f
codingIA-a <149007043+codingIA-a@users.noreply.github.com>
2023-11-22 20:46:37 -0300

    Update empleado.py

--- empleado.py
+++ empleado.py
@@ -1,9 +1,3 @@
-#crear una clase empleado que tenga los siguientes atributos:
-#nombre, id empleado, cargo, salario, departamento, tareas, bonos
-#ademas deberá tener los siguientes métodos:
-#asignar tarea, completar tareas, mostrar tareas, mostrar info empleado
-
-
 class Empleado:
     def __init__(self, nombre, id_empleado, cargo, salario, departamento):
         self.nombre = nombre
@@ -13,17 +7,41 @@ class Empleado:
         self.departamento = departamento
         self.tareas = []
         self.bonos = 0
-    #método asignar tarea
+
     def asignar_tarea(self, tarea):
         self.tareas.append(tarea)
-        print(f'Tarea asignada a {self.nombre} : {tarea}\n')
-    
-    #método mostrar tareas
+        print(f'Tarea asignada a {self.nombre}: {tarea}\n')
+
     def mostrar_tareas(self):
         print(f'Tareas asignadas a {self.nombre}:\n')
         for tarea in self.tareas:
             print(f'-{tarea}')
 
-empleado1 = Empleado('Adán', 1 , 'Desarrollador', 1000, 'TI')
+    def completar_tareas(self, tarea):
+        if tarea in self.tareas:
+            self.tareas.remove(tarea)
+            print(f'{self.nombre.capitalize()} completó la tarea: {tarea}\n')
+            self.bonos += self.calcular_bonos_por_tarea()
+            
+        else:
+            print(f'{self.nombre} no tiene asignada la tarea: {tarea}')
+
+    def mostrar_info(self):
+        print('*******Info******')
+        print(f'Nombre: {self.nombre.capitalize()}')
+        print(f'Bonos: {self.bonos}')
+        print(f'Salario: {self.salario}')
+        print(f'Salario + bonos: {self.salario+self.bonos}')
+
+    @staticmethod
+    def calcular_bonos_por_tarea():
+        return 10  # Puedes ajustar la cantidad de bonos según tus necesidades
+
+# Ejemplo de uso
+empleado1 = Empleado('Adán', 1, 'Desarrollador', 1000, 'TI')
 empleado1.asignar_tarea('Formulario Login')
+empleado1.asignar_tarea('Formulario Registro')
 empleado1.mostrar_tareas()
+empleado1.completar_tareas('Formulario Login')
+empleado1.completar_tareas('Formulario Registro')
+empleado1.mostrar_info()
